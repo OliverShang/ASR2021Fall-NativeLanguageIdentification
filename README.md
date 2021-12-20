8ffb8d3cffccb13b3bf1967bc3a354ac3a92c1e3
Mizuki_1950641 <70007256+Aeroraven@users.noreply.github.com>
2021-12-21 01:24:53 +0800

    Feature: Model Integration

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # congenial-adventure
 Final project for ASR2021@TongjiSSE
+
+#### Models
+CRNN+Integration: main_aesrc_model.py / SimpleAESRC
+CNN: simple_cnn_classifier.py / SimpleARCNN
+
+
+#### Todo
+Dataset & Dataloader
+Training
+Analysis
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from shared_encoder import SharedEncoder
 import torch
+from torchsummary import summary
 
-net = SharedEncoder(in_channels=3, hidden_dim=256)
-i = torch.autograd.Variable(torch.randn(5, 3, 224, 224))
-i2 = net(i)
-print(i2.shape)
+if __name__ == "__main__":
+    net = SharedEncoder(in_channels=1, hidden_dim=256).to("cpu")
+    summary(net,(1,1200,80),device="cpu")
